7c4aaaafd69be8c0e7cffe62b506b0713f34176d
imranime <m.imran@me.com>
2014-04-20 12:29:00 +0400

    Revert "Project Started"
    This reverts commit d3db547925aea33d5eccf95a5e99a1eb63e5c481.

--- ServerAPI/SOAP/EventoServerAPIConstants.h
+++
@@ -1,9 +0,0 @@
-/*Created by Muhammad Imran on 4/6/14. */
-
-#define kBaseServerURL                      @"http://213.42.55.45/"
-#define kCommonUnableToAccessServices       @"kCommonUnableToAccessServices"
-#define kCommonSessionTimeout               @"kCommonSessionTimeout"
-#define kAuthorizationFailureNotification   @"kAuthorizationFailureNotification"
-#define kCommonUnexpectedServerError        @"kCommonUnexpectedServerError"
-
-
