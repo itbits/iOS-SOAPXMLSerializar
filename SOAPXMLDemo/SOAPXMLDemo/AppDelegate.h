cacb58e0cf36a16f02bebe2b69af0f9aa76c8f5d
imranime <m.imran@me.com>
2014-04-21 17:59:51 +0400

    Started Demo Project
    Started Demo Project

---
+++ SOAPXMLDemo/SOAPXMLDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SOAPXMLDemo
+//
+//  Created by Muhammad Imran on 4/21/14.
+//  Copyright (c) 2014 Muhammad Imran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SOAPXMLDemo/SOAPXMLDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SOAPXMLDemo
+//
+//  Created by Muhammad Imran on 4/21/14.
+//  Copyright (c) 2014 Muhammad Imran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
